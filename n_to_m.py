63d40c4c4045ee6491d9b7738f444cd361fce25a
Jack Skiles <jackskiles420@gmail.com>
2020-04-02 11:23:50 -0400

    Most final changes to n_to_m.py

--- n_to_m.py
+++ n_to_m.py
@@ -1,12 +1,12 @@
 
-
+# Asks user for starting number and ending number to loop through.
 starting_number = input('Please input the number you would like to start at: ')
 starting_number = int(starting_number)
 
 ending_number = input('Please input the number you would like to end on: ')
 ending_number = int(ending_number)
 
+# Prints starting number + 1 as long as it is less than the ending number entered.
 while starting_number <= ending_number:
     print(starting_number)
     starting_number += 1
-    
