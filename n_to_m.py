476376fd12e47068baeb3ad75324404e9d51bfee
Jack Skiles <jackskiles420@gmail.com>
2020-04-02 11:22:10 -0400

    Changes to n_to_m.py

--- n_to_m.py
+++ n_to_m.py
@@ -5,3 +5,8 @@ starting_number = int(starting_number)
 
 ending_number = input('Please input the number you would like to end on: ')
 ending_number = int(ending_number)
+
+while starting_number <= ending_number:
+    print(starting_number)
+    starting_number += 1
+    
