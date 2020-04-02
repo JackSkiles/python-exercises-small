b421c845179864539e876cb87d63f014264c434d
Jack Skiles <jackskiles420@gmail.com>
2020-04-02 11:19:09 -0400

    created counter that takes user input

---
+++ n_to_m.py
@@ -0,0 +1,7 @@
+
+
+starting_number = input('Please input the number you would like to start at: ')
+starting_number = int(starting_number)
+
+ending_number = input('Please input the number you would like to end on: ')
+ending_number = int(ending_number)
