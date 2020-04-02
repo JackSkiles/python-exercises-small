d59589c50bea7e8c6c6b80c8b26fc7b3550b7ca4
Jack Skiles <jackskiles420@gmail.com>
2020-04-02 11:13:44 -0400

    counter from 1 to 10

---
+++ 1_to_10.py
@@ -0,0 +1,8 @@
+
+#creates counter variable to start at
+count = 1
+
+#creates while loop that will increase counter by 1 and print it until it reaches 10.
+while count <= 10:
+    print(count)
+    count += 1
